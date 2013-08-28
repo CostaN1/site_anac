8e8df44cb8343390b8e1d7641d479c049230310b
Camarent <flyno5@rambler.ru>
2013-08-29 01:26:25 +0400

    Merge remote-tracking branch 'origin/master' into kirik
    Conflicts:
    	anacondaz/img/live2.jpg
    	anacondaz/index.html
    	anacondaz/live.html
    	anacondaz/style.css

---
+++ anacondaz/js/primary.js
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: flyno_000
+ * Date: 29.08.13
+ * Time: 0:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+$("div.albums").children().mouseover(function() {
+        $(this).stop(1).animate({width:'41.6667%'},300)
+
+    }).mouseout(function() {
+        $(this).stop(1).animate({width:'33.33334%'},300);
+    });
